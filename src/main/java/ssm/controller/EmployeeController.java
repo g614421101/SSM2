aa93b29db498910f8e2d38c5176306df4dbf5aa8
g614421101 <95484363+g614421101@users.noreply.github.com>
2022-02-17 15:26:31 +0800

    pull commit

--- src/main/java/ssm/controller/EmployeeController.java
+++ src/main/java/ssm/controller/EmployeeController.java
@@ -25,6 +25,7 @@ public class EmployeeController {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
         return "list";
     }
 }
